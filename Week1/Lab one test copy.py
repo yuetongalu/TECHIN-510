dcbf9b68aad6fb9fa38eac8dca96016dcf2651cc
yuetongalu <yuetongl@uw.edu01~>
2024-01-24 07:58:43 +0800

    20240124test1 before final assignment

---
+++ Week1/Lab one test copy.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+#st.header('Yuetong Lu', divider='black')
+st.header('Yuetong Lu', divider='rainbow')
+st.image('https://i.postimg.cc/7h07RC9R/Yuetong-Selfie.jpg', caption='Me')
+st.write('Born in Guangzhou, China, in 2000, I underwent traditional education training common to my peers in mainland China before my admission to the School of Visual Arts in New York City. My upbringing within Chinese culture, history, Communism, and Marxism has significantly influenced my ideas and artistic approach. My works predominantly explore cultural and political themes, centering on the complex interplay between individuals and society and reflecting my preoccupation with modern-day existence.')      
---
+++ Week3/week3.py
@@ -0,0 +1,4 @@
+import sqlite3
+con = sqlite3.connect("week3.sqlite", isolation_level=None)
+cur = con.cursor()
+
